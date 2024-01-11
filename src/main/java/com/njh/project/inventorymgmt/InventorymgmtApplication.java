249a935a0047f46a91c097a75e46d91aff6da2fa
노종현 <beatz148@gmail.com>
2024-01-11 22:09:59 +0900

    initialize

---
+++ src/main/java/com/njh/project/inventorymgmt/InventorymgmtApplication.java
@@ -0,0 +1,13 @@
+package com.njh.project.inventorymgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorymgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorymgmtApplication.class, args);
+	}
+
+}
