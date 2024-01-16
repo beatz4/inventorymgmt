2aa8e672e06c54ba2e4fc456e8d8dc299374fad9
노종현 <beatz148@gmail.com>
2024-01-16 22:38:27 +0900

    메인프레임, 문서 작성, 그리드 라이브러리 추가

---
+++ src/main/java/com/njh/project/inventorymgmt/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.njh.project.inventorymgmt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class OrderController {
+    
+    @GetMapping("/ordermgmt")
+    public String getOrderView() {
+        return "/ordermgmt";
+    }
+    
+}
---
+++ src/main/resources/static/js/companymgmt.js
@@ -0,0 +1,24 @@
+
+
+class CompanyMgmt {
+
+    constructor(config) {
+
+        this.config = {
+            grid: config.grid
+        };
+
+        this.initGrid();
+    }
+
+    initGrid() {
+
+        const grid = new tui.Grid({
+            el: document.getElementById(this.config.grid), // 컨테이너 엘리먼트
+            columns: [ 
+                // ...,
+            ],
+        // ...,
+        });
+    }
+}
